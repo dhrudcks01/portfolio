ada13fdb06186b38f3119e8328f2ebc3298854d1
GyeongChanOH <dhrudcks01@gmail.com>
2022-08-01 23:19:47 +0900

    Fix: Error

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,62 +1,67 @@
-import React from "react";
-import myImage from "../img/myImage.png";
+import React from 'react';
+import myImage from '../img/myImage.png';
 
 class About extends React.Component {
   constructor() {
     super();
     this.state = {
       skills: [
-        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
-        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
+        { id: 'HTML5_skill', content: 'HTML5', porcentage: '65%', value: '65' },
+        { id: 'CSS3_skill', content: 'CSS3', porcentage: '65%', value: '65' },
         {
-          id: "JavaScript_skill",
-          content: "JavaScript",
-          porcentage: "90%",
-          value: "90"
-        },
-        { id: "PHP_skill", content: "PHP", porcentage: "70%", value: "70" },
-        {
-          id: "ReactJS_skill",
-          content: "ReactJS",
-          porcentage: "80%",
-          value: "80"
+          id: 'JavaScript_skill',
+          content: 'JavaScript',
+          porcentage: '75%',
+          value: '75',
         },
         {
-          id: "Python_skill",
-          content: "Python",
-          porcentage: "75%",
-          value: "75"
+          id: 'NodeJS_skill',
+          content: 'NodeJS',
+          porcentage: '80%',
+          value: '80',
         },
         {
-          id: "VanillaJS_skill",
-          content: "VanillaJS",
-          porcentage: "85%",
-          value: "85"
+          id: 'ReactJS_skill',
+          content: 'ReactJS',
+          porcentage: '70%',
+          value: '70',
         },
         {
-          id: "Wordpress_skill",
-          content: "Wordpress",
-          porcentage: "80%",
-          value: "80"
-        }
+          id: 'Python_skill',
+          content: 'Python',
+          porcentage: '30%',
+          value: '30',
+        },
+        {
+          id: 'VanillaJS_skill',
+          content: 'VanillaJS',
+          porcentage: '70%',
+          value: '70',
+        },
+        {
+          id: 'Java_skill',
+          content: 'Java',
+          porcentage: '30%',
+          value: '30',
+        },
       ],
       about_me: [
         {
-          id: "first-p-about",
+          id: 'first-p-about',
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            'Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis.',
         },
         {
-          id: "second-p-about",
+          id: 'second-p-about',
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            'Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis.',
         },
         {
-          id: "third-p-about",
+          id: 'third-p-about',
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
-        }
-      ]
+            'Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis.',
+        },
+      ],
     };
   }
 
@@ -72,11 +77,11 @@ class About extends React.Component {
                     <div className="row">
                       <div
                         className="col-sm-6 col-md-5"
-                        style={{ margin: "0 auto" }}
+                        style={{ margin: '0 auto' }}
                       >
                         <div
                           className="about-img"
-                          style={{ textAlign: "center" }}
+                          style={{ textAlign: 'center' }}
                         >
                           <img
                             className="img-fluid rounded b-shadow-a"
@@ -87,10 +92,10 @@ class About extends React.Component {
                     </div>
                     <div className="skill-mf">
                       {/* <p className="title-s">Skill</p> */}
-                      {this.state.skills.map(skill => {
+                      {this.state.skills.map((skill) => {
                         return (
                           <React.Fragment key={skill.id}>
-                            <span>{skill.content}</span>{" "}
+                            <span>{skill.content}</span>{' '}
                             <span className="pull-right">
                               {skill.porcentage}
                             </span>
@@ -114,7 +119,7 @@ class About extends React.Component {
                       <div className="title-box-2">
                         <h5 className="title-left">About Me</h5>
                       </div>
-                      {this.state.about_me.map(content => {
+                      {this.state.about_me.map((content) => {
                         return (
                           <p className="lead" key={content.id}>
                             {content.content}
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./stars.scss";
-import Typed from "react-typed";
+import React from 'react';
+import './stars.scss';
+import Typed from 'react-typed';
 
 class Intro extends React.Component {
   render() {
@@ -14,16 +14,12 @@ class Intro extends React.Component {
         <div className="intro-content display-table">
           <div className="table-cell">
             <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am XYZ</h1>
+              <h1 className="intro-title mb-4">Hello, I am Oh GyeongChan</h1>
               <p className="intro-subtitle">
                 <span className="text-slider-items"></span>
                 <strong className="text-slider">
                   <Typed
-                    strings={[
-                      "Front End Developer",
-                      "Back End Developer",
-                      "Software Engineer"
-                    ]}
+                    strings={['Back End Developer', 'Software Engineer']}
                     typeSpeed={80}
                     backDelay={1100}
                     backSpeed={30}
