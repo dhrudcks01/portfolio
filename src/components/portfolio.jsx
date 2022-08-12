15e9e6714ed12cb2ef3b7eb14ba2643d0fd75acc
GyeongChanOH <dhrudcks01@gmail.com>
2022-08-12 16:50:13 +0900

    fix

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -30,8 +30,8 @@ class Portfolio extends React.Component {
               <div className="title-box text-center">
                 <h3 className="title-a">Portfolio</h3>
                 <p className="subtitle-a">
-                  Incididunt nostrud id aute culpa excepteur pariatur consequat
-                  elit culpa nulla enim anim incididunt.
+                  {/* Incididunt nostrud id aute culpa excepteur pariatur consequat
+                  elit culpa nulla enim anim incididunt. */}
                 </p>
                 <div className="line-mf"></div>
               </div>
